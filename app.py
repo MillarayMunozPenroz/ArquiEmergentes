f1436e94adcf817fe2c7038ee211f2f6584810ee
MillarayMunozPenroz <millaray.munoz_p@mail.udp.cl>
2024-07-14 01:35:55 -0400

    creación de base

--- app.py
+++ app.py
@@ -12,12 +12,55 @@ def get_db_connection():
 def init_db():
     conn = get_db_connection()
     conn.execute('''
-    CREATE TABLE IF NOT EXISTS iot_data (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-        data TEXT
+    CREATE TABLE IF NOT EXISTS Admin (
+        Username STRING PRIMARY KEY,
+        Password STRING NOT NULL
     )
     ''')
+
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS Company(
+        ID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+        Company_name STRING NOT NULL,
+        Company_api_key STRING NOT NULL
+    )
+    ''')
+
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS Location(
+        ID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+        company_id INT NOT NULL,
+        location_name STRING NOT NULL,
+        location_country STRING NOT NULL,
+        location_city STRING NOT NULL,
+        location_meta STRING NOT NULL,
+        FOREIGNKEY (company_id) REFERENCES Company(ID)
+    )
+    ''')
+
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS Sensor(
+        location_id INT NOT NULL,
+        sensor_id INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+        sensor_name STRING NOT NULL,
+        sensor_category STRING NOT NULL,
+        sensor_meta STRING NOT NULL,
+        sensor_api_key STRING NOT NULL,
+        FOREIGNKEY (location_id) REFERENCES Location(ID)
+    )
+    ''')
+
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS Sensor_Data(
+        ID INT PRIMARY KEY AUTOINCREMENT NOT NULL,
+        sensor_id INT NOT NULL,
+        tiempo DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGNKEY (sensor_id) REFERENCES Sensor(sensor_id))
+    )
+    ''')
+
+
+
     conn.commit()
     conn.close()
 
